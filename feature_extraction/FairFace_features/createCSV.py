b50e209089eae9fc9dbf5eded40e0d5b74f8eca7
JanLeyva <janleyvamassague@gmail.com>
2022-05-28 13:53:47 +0200

    update ff

--- feature_extraction/FairFace_features/createCSV.py
+++ feature_extraction/FairFace_features/createCSV.py
@@ -1,16 +1,20 @@
 # We create the csv file to be used in FairFace
 import pandas as pd
+import argparse
 import os
 
 
-def createCSV(path = "/Content/Hateful_memes/img", outpath = "/content/FairFace"):
-	imagesList=os.listdir(path)
-	csvImages = pd.DataFrame(imagesList)
+parser = argparse.ArgumentParser(description='scrip to create csv file to be used in FairFace. Output csvImg.csv')
+parser.add_argument("--path", help="path where images are stored.")
+parser.add_argument("--outpath", help = "output path for the .csv")
+args = parser.parse_args()
 
-	csvImg = []
-	for i in range(len(csvImages)):
-	  csvImg.append(os.path.join("/content", path, csvImages.at[i,0]))
+imagesList=os.listdir(args.path)
+csvImages = pd.DataFrame(imagesList)
 
-	csvImg = pd.DataFrame(csvImg).rename(columns={0: "img_path"})
-	csvImg.to_csv(os.path.join(outpath, "csvImg.csv"),index=False)
+csvImg = []
+for i in range(len(csvImages)):
+	csvImg.append(os.path.join("/content", args.path, csvImages.at[i,0]))
 
+csvImg = pd.DataFrame(csvImg).rename(columns={0: "img_path"})
+csvImg.to_csv(os.path.join(args.outpath, "csvImg.csv"),index=False)
