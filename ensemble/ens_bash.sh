2a1ef3f07984cf1e13000a809691be21c8bff658
JanLeyva <janleyvamassague@gmail.com>
2022-06-30 15:19:08 +0200

    add fairface join

---
+++ ensemble/ens_bash.sh
@@ -0,0 +1,2 @@
+#bash -x ./env.sh
+bash python ens_v3.py --enspath ./results/ --fileFairface fairface21062022.json --enstype sa --exp ENS3 --meme_anno_path ./annotations/test_unseen.jsonl --racism_rule True 
