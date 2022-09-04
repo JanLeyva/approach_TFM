55ef95f058392829eb683157e62a25ba64d062d8
JanLeyva <janleyvamassague@gmail.com>
2022-09-04 23:52:35 +0200

    add grid-search mmf

---
+++ mmf-models/grid-search/grid-search.py
@@ -0,0 +1,50 @@
+import subprocess
+from subprocess import call
+import os
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # parser.add_argument("-home", "--home",  help="home directory.")
+    parser.add_argument("-iter", "--iter",  default = 0, help="number interation to star.")
+
+    # Parse the arguments.
+    args = parser.parse_args()
+    return args
+
+# Assign corresponding variables
+# home = args.home
+
+
+def generate_list():
+    confi_dotlist=[]
+    i = 0
+    for lr in [0.3, 0.6]:
+        for w_type in ['warmup_cosine', 'warmup_linear']:
+            for w_factor in [0.1, 0.3]:
+                # call the bash script which start the training
+                confi_dotlist.append({
+                    "iteration": i,
+                    "lr": lr,
+                    "w_type": w_type,
+                    "w_factor": w_factor})
+                i+=1
+
+
+    return confi_dotlist
+
+
+
+def main(iter=0):
+    # for each iteration a folder will be created whose name is 'experiemnt_{i}'
+    confi_dotlist=generate_list()
+    confi_dotlist=confi_dotlist[iter:]
+    for e, it in enumerate(confi_dotlist):
+        os.mkdir("experiment_{}".format(confi_dotlist[e]['iteration']))
+        rc = call(f"./grid-serarch.sh {experiement_ + str(i)} {confi_dotlist[e]['lr']} {confi_dotlist[e]['w_type']} {confi_dotlist[e]['w_factor']} ", shell=True)
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args.iter)
---
+++ mmf-models/grid-search/grid-search.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+export OC_DISABLE_DOT_ACCESS_WARNING=1
+
+
+
+mmf_run config="projects/visual_bert/configs/hateful_memes/from_coco.yaml" \
+        model="visual_bert" \
+        dataset=hateful_memes \
+        run_type=train_val \
+        checkpoint.max_to_keep=1 \
+        checkpoint.resume_zoo=visual_bert.pretrained.cc.full \
+        training.tensorboard=True \
+        training.checkpoint_interval=50 \
+        training.evaluation_interval=50 \
+        training.max_updates=2000 \
+        training.log_interval=100 \
+        dataset_config.hateful_memes.max_features=100 \
+        training.lr_ratio=$2 \
+        training.use_warmup=True \
+        training.warmup_factor=$5 \
+        scheduler.type=$3 \
+        training.batch_size=32 \
+        optimizer.params.lr=5.0e-05 \
+        scheduler.params.num_training_steps=1000 \
+        env.save_dir=./$1 \
+        env.tensorboard_logdir=logs/fit/$1 \
